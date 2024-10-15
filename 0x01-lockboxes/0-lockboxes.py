fc59c490ea693bb22022aee16f01a3d9997e9542
Awalu-Levison <levisonawalu251@gmail.com>
2024-10-15 02:19:22 +0000

    determines if all the boxes can be opened.

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Unlocking the list of lists"""
+
+
+def canUnlockAll(boxes):
+    """This function defines the list of lists
+    the list contains key based on particular box
+    """
+
+    keys = [0]
+    """Denotes the first box in the list"""
+
+    for mykey in keys:
+        """iterate through the list"""
+        for myboxkey in boxes[mykey]:
+            """check if the particular key matches
+            with the particular box
+            """
+            if myboxkey not in keys and myboxkey < len(boxes):
+                mykey.append(boxes)
+    if len(mykey) == len(boxes):
+        return True
+    return False
