67c858dade1ef31dc1167af70d5500130e737a46
Awalu-Levison <levisonawalu251@gmail.com>
2024-10-18 01:25:57 +0000

    Added a readme file

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,22 +4,21 @@
 
 def minOperations(n):
     """
-    minOperations
-    Gets fewest # of operations needed to result in exactly n H characters
+    Gets fewest number of operations
     """
     # Maximum of two numbers: (min, Copy All => Paste)
     if (n < 2):
         return 0
-    ops, root = 0, 2
+    sym, root = 0, 2
     while root <= n:
 
         # check evenneness of a number
         if n % root == 0:
-            ops += root
+            sym += root
             # set n to the remainder
             n = n / root
             # find remaining smaller vals that evenly-divide n
             root -= 1
         # increment root  until  become evenness
         root += 1
-    return ops
+    return sym
