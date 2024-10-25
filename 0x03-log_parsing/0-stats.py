0f47f69b50e89562b11371145e853e99cd0fde84
Awalu-Levison <levisonawalu251@gmail.com>
2024-10-25 01:25:07 +0000

    Updated some files

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """
-The Log parsing
+Log parsing
 """
 
 import sys
 
-
 if __name__ == '__main__':
 
-    f_size, count = 0, 0
+    filesize, count = 0, 0
     codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     stats = {k: 0 for k in codes}
 
     def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(f_size))
+        print("File size: {:d}".format(filesize))
         for k, v in sorted(stats.items()):
             if v:
                 print("{}: {}".format(k, v))
@@ -29,12 +28,12 @@ if __name__ == '__main__':
             except BaseException:
                 pass
             try:
-                f_size += int(data[-1])
+                filesize += int(data[-1])
             except BaseException:
                 pass
             if count % 10 == 0:
-                print_stats(stats, f_size)
-        print_stats(stats, f_size)
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
     except KeyboardInterrupt:
-        print_stats(stats, f_size)
+        print_stats(stats, filesize)
         raise
