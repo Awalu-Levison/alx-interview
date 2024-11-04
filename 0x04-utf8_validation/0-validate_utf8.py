4ccf0db5629b494ac2f5b3c9c535693ba25e6fd0
Awalu-Levison <levisonawalu251@gmail.com>
2024-11-04 01:36:27 +0000

    UTF-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""Interpreter directive"""
+"""UTF-8 Validation"""
+
+
+def bit_len(num):
+    """Find the bit length"""
+    my_bits = 0
+    bit_len = 1 << 7
+    while bit_len & num:
+        my_bits += 1
+        bit_len = bit_len >> 1
+    return my_bits
 
 
 def validUTF8(data):
-    """
-    Extract list significant bits
-    in utf-8 data encoding
-    """
-    lsb = data & 0xFF
-
-    try:
-        data.decode('utf-8')
-        return True
-    except UnicodeDecodeError:
-        return False
-
-    with open('my_file.txt', 'rb') as file:
-        content = file.read()
-        if validUTF8(content):
-            return True
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    my_count = 0
+    for i in range(len(data)):
+        if my_count == 0:
+            my_count = get_leading_set_bits(data[i])
+            if my_count == 0:
+                continue
+            if my_count == 1 or my_count > 4:
+                return False
         else:
-            return False
+            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
+                return False
+        my_count -= 1
+    return my_count == 0
